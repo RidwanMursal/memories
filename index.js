f7efd2b8ab948aa821eaf7773fea8a2600f301de
ridwanmursal <ridwan09@hotmail.com>
2022-06-25 16:23:57 -0400

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log("hello worldd")
 const express = require("express")
 const router = require("./routes/posts.js")
+require("dotenv").config()
 
 const app = express()
 
@@ -9,4 +10,4 @@ app.use("/posts", router)
 app.get("/", (req, res) => res.send("hello"))
 
 
-app.listen(5050, () => console.log("i am listening"))
+app.listen(process.env.PORT, () => console.log("i am listening on port", process.env.PORT))
